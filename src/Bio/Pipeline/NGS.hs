6f51e476a62b3cedf5e24834311317c84cbd8e8f
Kai Zhang <kai@kzhang.org>
2017-08-29 12:55:19 -0700

    update types

--- src/Bio/Pipeline/NGS.hs
+++ src/Bio/Pipeline/NGS.hs
@@ -36,13 +36,12 @@ module Bio.Pipeline.NGS
     , rsemMkIndex
     , rsemQuant
 
-    , nameWith
+    , mapFileWithDefName
     ) where
 
 import           Bio.Data.Experiment
 import           Control.Lens
-import           Data.Either                 (fromLeft, fromRight)
-import Data.Bitraversable (bitraverse)
+import           Data.Bitraversable          (bitraverse)
 import           Data.Promotion.Prelude.List (Delete, Elem, Insert)
 import           Data.Singletons             (SingI)
 import qualified Data.Text                   as T
@@ -59,50 +58,49 @@ bwaAlign :: ( tags1' ~ Delete 'Gzip tags1
          => (FilePath, String)
          -> FilePath
          -> BWAOptSetter
-         -> Either (ATACSeq (File tags1 'Fastq))
-                   (ATACSeq (File tags2 'Fastq, File tags2 'Fastq))
-         -> IO ( Either (ATACSeq (File tags1' 'Bam))
-                        (ATACSeq (File tags2' 'Bam)) )
+         -> Either (ATACSeq S (File tags1 'Fastq))
+                   (ATACSeq S (File tags2 'Fastq, File tags2 'Fastq))
+         -> IO ( Either (ATACSeq S (File tags1' 'Bam))
+                        (ATACSeq S (File tags2' 'Bam)) )
 bwaAlign (dir, suffix) idx opt = bitraverse fun1 fun2
   where
-    fun1 = nameWith (dir++"/") suffix $ \output input ->
-        bwaAlign_ output idx opt $ Left input
-    fun2 = nameWith (dir++"/") suffix $ \output input ->
-        bwaAlign_ output idx opt $ Right input
+    fun1 = mapFileWithDefName (dir++"/") suffix $ \output ->
+        bwaAlign_ output idx opt . Left
+    fun2 = mapFileWithDefName (dir++"/") suffix $ \output ->
+        bwaAlign_ output idx opt . Right
 
 filterBam :: ( SingI tags, Experiment experiment
              , tags' ~ (Insert 'Sorted tags) )
           => (FilePath, String)
-          -> experiment (File tags 'Bam)
-          -> IO (experiment (File tags' 'Bam))
-filterBam (dir, suffix) = nameWith (dir++"/") suffix filterBam_
+          -> experiment S (File tags 'Bam)
+          -> IO (experiment S (File tags' 'Bam))
+filterBam (dir, suffix) = mapFileWithDefName (dir++"/") suffix filterBam_
 
 removeDuplicates :: (Experiment experiment, SingI tags)
                  => FilePath   -- ^ picard
                  -> (FilePath, String)
-                 -> experiment (File tags 'Bam)
-                 -> IO (experiment (File tags 'Bam))
-removeDuplicates picard (dir, suffix) = nameWith (dir++"/") suffix
+                 -> experiment S (File tags 'Bam)
+                 -> IO (experiment S (File tags 'Bam))
+removeDuplicates picard (dir, suffix) = mapFileWithDefName (dir++"/") suffix
     (removeDuplicates_ picard)
 
 bamToBed :: ( Experiment experiment, SingI tags
             , Elem 'Sorted tags ~ 'True )
          => (FilePath, String)
-         -> experiment (File tags 'Bam)
-         -> IO (experiment (File (Insert 'Gzip tags) 'Bed))
-bamToBed (dir, suffix) = nameWith (dir ++ "/") suffix fn
-  where
-    fn output fl = if fl `hasTag` Pairend
+         -> experiment S (File tags 'Bam)
+         -> IO (experiment S (File (Insert 'Gzip tags) 'Bed))
+bamToBed (dir, suffix) = mapFileWithDefName (dir ++ "/") suffix $ \output fl ->
+    if fl `hasTag` Pairend
         then bam2Bed_ output (const True) fl
         else bam2BedPE_ output (const True) fl
 
 concatBed :: (SingI tags, Experiment experiment)
           => (FilePath, String)
-          -> experiment (File tags 'Bed)
-          -> IO (experiment (File (Insert 'Gzip tags) 'Bed))
+          -> experiment N (File tags 'Bed)
+          -> IO (experiment S (File (Insert 'Gzip tags) 'Bed))
 concatBed (dir, suffix) e = do
     fl <- concatBed_ output fls
-    return $ e & replicates .~ [ Replicate fl [] 0 ]
+    return $ e & replicates .~ return (Replicate fl [] 0)
   where
     fls = e^..replicates.folded.files
     output = printf "%s/%s_rep0%s" dir (T.unpack $ e^.eid) suffix
@@ -113,42 +111,39 @@ starAlign :: ( SingI tags1, SingI tags2, Elem 'Pairend tags2 ~ 'True
           => (FilePath, String)
           -> FilePath            -- ^ STAR genome index
           -> STAROptSetter       -- ^ Options
-          -> Either (RNASeq (File tags1 'Fastq))
-                    (RNASeq (File tags2 'Fastq, File tags2 'Fastq))
-          -> IO ( Either (RNASeq (File tags1' 'Bam, File tags1' 'Bam))
-                         (RNASeq (File tags2' 'Bam, File tags2' 'Bam)) )
+          -> Either (RNASeq S (File tags1 'Fastq))
+                    (RNASeq S (File tags2 'Fastq, File tags2 'Fastq))
+          -> IO ( Either (RNASeq S (File tags1' 'Bam, File tags1' 'Bam))
+                         (RNASeq S (File tags2' 'Bam, File tags2' 'Bam)) )
 starAlign (dir, suffix) idx setter = bitraverse fun1 fun2
   where
-    fun1 = nameWith (dir++"/") "" $ \output input -> starAlign_
+    fun1 = mapFileWithDefName (dir++"/") "" $ \output -> starAlign_
         (output ++ "_genome" ++ suffix) (output ++ "_anno" ++ suffix)
-        idx setter $ Left input
-    fun2 = nameWith (dir++"/") "" $ \output input -> starAlign_
+        idx setter . Left
+    fun2 = mapFileWithDefName (dir++"/") "" $ \output -> starAlign_
         (output ++ "_genome" ++ suffix) (output ++ "_anno" ++ suffix)
-        idx setter $ Right input
+        idx setter . Right
 
 rsemQuant :: SingI tags
           => FilePath
           -> FilePath
           -> RSEMOptSetter
-          -> RNASeq (File tags 'Bam)
-          -> IO (RNASeq (File '[GeneQuant] 'Tsv, File '[TranscriptQuant] 'Tsv))
-rsemQuant dir idx setter e = e & replicates.traverse %%~
-    ( \r -> r & files %%~ fun (e^.eid) (r^.number) )
-  where
-    fun id' rep = rsemQuant_ outputPrefix idx setter
-      where
-        outputPrefix = printf "%s/%s_rep%d" dir (T.unpack id') rep
+          -> RNASeq S (File tags 'Bam)
+          -> IO (RNASeq S (File '[GeneQuant] 'Tsv, File '[TranscriptQuant] 'Tsv))
+rsemQuant dir idx setter = mapFileWithDefName (dir++"/") "" $ \output ->
+    rsemQuant_ output idx setter
 
-nameWith :: (Experiment experiment, Monad m)
-         => String   -- ^ Prefix
-         -> String   -- ^ Suffix
-         -> (FilePath -> file -> m file')
-         -> experiment file
-         -> m (experiment file')
-nameWith prefix suffix fn e = e & replicates.traverse %%~ (\r -> r & files %%~ f r)
+mapFileWithDefName :: (Experiment experiment, Monad m, Traversable t)
+                   => String   -- ^ Prefix
+                   -> String   -- ^ Suffix
+                   -> (FilePath -> file -> m file')
+                   -> experiment t file
+                   -> m (experiment t file')
+mapFileWithDefName prefix suffix fn e = e & replicates.traverse %%~
+    (\r -> r & files %%~ f r)
   where
     f r fl = fn output fl
       where
         output = printf "%s%s_rep%d%s" prefix (T.unpack $ e^.eid)
             (r^.number) suffix
-{-# INLINE nameWith #-}
+{-# INLINE mapFileWithDefName #-}
