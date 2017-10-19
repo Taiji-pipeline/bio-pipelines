f59caa9f7bffececff5576ec7980696c639f00f4
Kai Zhang <kai@kzhang.org>
2017-10-19 01:21:16 -0700

    add downloadFiles

--- src/Bio/Pipeline/Download.hs
+++ src/Bio/Pipeline/Download.hs
@@ -1,23 +1,27 @@
-{-# LANGUAGE DataKinds         #-}
-{-# LANGUAGE FlexibleContexts  #-}
-{-# LANGUAGE OverloadedStrings #-}
-module Bio.Pipeline.Download where
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE OverloadedStrings   #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+module Bio.Pipeline.Download (downloadFiles) where
 
 import           Bio.Data.Experiment
+import           Bio.Data.Experiment.File   (SFileType)
+import           Bio.Data.Experiment.Parser (guessFormat)
 import           Conduit
 import           Control.Lens
 import           Control.Monad
-import qualified Data.ByteString.Char8 as B
-import           Data.Coerce           (coerce)
-import           Data.List.Split       (splitOn)
+import qualified Data.ByteString.Char8      as B
+import           Data.Coerce                (coerce)
+import           Data.List.Split            (splitOn)
 import           Data.Maybe
-import           Data.Singletons       (SingI)
-import qualified Data.Text             as T
+import           Data.Singletons            (SingI, SomeSing (..), toSing,
+                                             withSingI)
+import qualified Data.Text                  as T
 import           Network.HTTP.Conduit
-import           Shelly                hiding (FilePath)
+import           Shelly                     hiding (FilePath)
 
 sraToFastq :: SingI tags
-           => FilePath
+           => FilePath    -- ^ Output directory
            -> File tags 'SRA
            -> IO (Either (File '[Gzip] 'Fastq)
                          (File '[Gzip] 'Fastq, File '[Gzip] 'Fastq)
@@ -61,68 +65,45 @@ sraToFastq outDir input = if input `hasTag` Pairend
                     run_ "rm" f2s
         return ( (coerce f1 :: File '[Gzip] 'Fastq)
                , (coerce f2 :: File '[Gzip] 'Fastq) )
+{-# INLINE sraToFastq #-}
 
-               {-
-downloadData :: FilePath
-             -> SomeFile
-             -> IO (Maybe SomeFile)
-downloadData outDir (SomeFile fl) = case () of
-    _ | getFileType fl == SRA -> if fl `hasTag` Pairend
-            then Right <$> fastqDumpPair fl
-            else Left <$> fastqDump fl
-      | fl `hasTag` ENCODE -> do
-  where
-    downloadfile fl = do
-      f1_name <- downloadENCODE (fl^.location) outDir
-      let f1 = location .~ f1_name $ fl
-      return $ if getFileType fl == Other
-          then let ft = guessFormat f1_name
-               in case toSing ft of
-                    SomeSing ft' -> withSingI ft' $ SomeFile $ setFiletype ft' f1
-          else SomeFile f1
-    fastqDump fl = do
-        let f1_name = outDir ++ "/" ++ fl^.location ++ ".fastq.gz"
-            f1 = location .~ f1_name $ fl
-        case splitOn "+" (fl^.location) of
-            [f] -> shelly $ run_ "fastq-dump" ["--origfmt", "-I", "--gzip",
-                "-O" ,T.pack outDir, T.pack f]
-            fs -> do
-                forM_ fs $ \f -> shelly $ run_ "fastq-dump" ["--origfmt", "-I",
-                    "-O", T.pack outDir, T.pack f]
-                let f1s = map (\x -> T.pack $ outDir ++ "/" ++ x ++ ".fastq") fs
-                shelly $ escaping False $ do
-                    run_ "cat" $ f1s ++ ["|", "gzip", "-c", ">", T.pack f1_name]
-                    run_ "rm" f1s
-        return $ SomeFile (coerce f1 :: File '[Gzip] 'Fastq)
-    fastqDumpPair fl = do
-        let f1_name = outDir ++ "/" ++ fl^.location ++ "_1.fastq.gz"
-            f2_name = outDir ++ "/" ++ fl^.location ++ "_2.fastq.gz"
-            f1 = location .~ f1_name $ fl
-            f2 = location .~ f2_name $ fl
+downloadFiles :: FilePath
+              -> Either SomeFile (SomeFile, SomeFile)
+              -> IO (Either SomeFile (SomeFile, SomeFile))
+downloadFiles outDir (Left (SomeFile fl))
+    | getFileType fl == SRA = if fl `hasTag` Pairend
+        then bimap SomeFile (bimap SomeFile SomeFile) <$>
+            sraToFastq outDir (coerce fl :: File '[Pairend] 'SRA)
+        else bimap SomeFile (bimap SomeFile SomeFile) <$>
+            sraToFastq outDir (coerce fl :: File '[] 'SRA)
+    | fl `hasTag` ENCODE = Left <$> downloadENCODE outDir (SomeFile fl)
+    | otherwise = return $ Left $ SomeFile fl
+downloadFiles outDir (Right (SomeFile f1, SomeFile f2))
+    | f1 `hasTag` ENCODE && f2 `hasTag` ENCODE = do
+        f1' <- downloadENCODE outDir (SomeFile f1)
+        f2' <- downloadENCODE outDir (SomeFile f2)
+        return $ Right (f1', f2')
+    | otherwise = return $ Right (SomeFile f1, SomeFile f2)
 
-        case splitOn "+" (fl^.location) of
-            [f] -> shelly $ run_ "fastq-dump" ["--origfmt", "-I",
-                "--split-files", "--gzip", "-O", T.pack outDir, T.pack f]
-            fs -> do
-                forM_ fs $ \f -> shelly $ run_ "fastq-dump" ["--origfmt", "-I",
-                    "--split-files", "-O", T.pack outDir, T.pack f]
-                let f1s = map (\x -> T.pack $ outDir ++ "/" ++ x ++ "_1.fastq") fs
-                    f2s = map (\x -> T.pack $ outDir ++ "/" ++ x ++ "_2.fastq") fs
-                shelly $ escaping False $ do
-                    run_ "cat" $ f1s ++ ["|", "gzip", "-c", ">", T.pack f1_name]
-                    run_ "rm" f1s
-                    run_ "cat" $ f2s ++ ["|", "gzip", "-c", ">", T.pack f2_name]
-                    run_ "rm" f2s
-        return ( SomeFile (coerce f1 :: File '[Gzip] 'Fastq)
-               , SomeFile (coerce f2 :: File '[Gzip] 'Fastq) )
-
-    -}
+downloadENCODE :: FilePath
+               -> SomeFile
+               -> IO SomeFile
+downloadENCODE outDir (SomeFile (fl :: File tags filetype)) = do
+    f_name <- downloadENCODE' (fl^.location) outDir
+    let newFile = location .~ f_name $ fl
+    return $ if getFileType fl == Other
+        then let ft = guessFormat f_name
+             in case toSing ft of
+                 SomeSing (ft' :: SFileType ft) -> withSingI ft' $ SomeFile
+                     (coerce newFile :: File tags ft)
+        else SomeFile newFile
+{-# INLINE downloadENCODE #-}
 
 -- | Download data from ENCODE portal to a given directory.
-downloadENCODE :: String    -- ^ Accession number
-               -> FilePath  -- ^ Output dir
-               -> IO FilePath
-downloadENCODE acc dir = do
+downloadENCODE' :: String    -- ^ Accession number
+                -> FilePath  -- ^ Output dir
+                -> IO FilePath
+downloadENCODE' acc dir = do
      request <- parseRequest url
      manager <- newManager tlsManagerSettings
      runResourceT $ do
@@ -134,3 +115,4 @@ downloadENCODE acc dir = do
          return $ dir ++ "/" ++ filename
   where
     url = "https://www.encodeproject.org/files/" ++ acc ++ "/@@download"
+{-# INLINE downloadENCODE' #-}
