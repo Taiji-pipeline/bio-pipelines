08655609dd6fcccefa74b0da5098d91372f0598a
Kai Zhang <kai@kzhang.org>
2018-05-15 10:35:30 -0700

    add IsString instance for Directory

--- src/Bio/Pipeline/Utils.hs
+++ src/Bio/Pipeline/Utils.hs
@@ -11,11 +11,15 @@ module Bio.Pipeline.Utils
 import           Control.Monad.IO.Class
 import           Data.Aeson             (FromJSON (..), ToJSON (..))
 import           Data.Serialize         (Serialize (..))
+import           Data.String            (IsString (..))
 import qualified Data.Text              as T
 import           GHC.Generics           (Generic)
 import           Shelly                 (fromText, mkdir_p, shelly)
 
 newtype Directory = Directory FilePath deriving (Show, Read, Ord, Eq, Generic)
+
+instance IsString Directory where
+    fromString = Directory
 
 instance Monoid Directory where
     mempty = Directory ""
@@ -31,9 +35,11 @@ instance Serialize Directory
 
 asDir :: FilePath -> Directory
 asDir = Directory
+{-# INLINE asDir #-}
 
 -- | Return the path to the directory. This will create a new directory if it
 -- doesn't exist.
 getPath :: MonadIO m => Directory -> m FilePath
 getPath (Directory dir) = liftIO $ shelly $
     mkdir_p (fromText $ T.pack dir) >> return dir
+{-# INLINE getPath #-}
