b9814899ac5d8322bf1e8e04b48712472e807df7
Kai Zhang <kai@kzhang.org>
2019-06-02 16:33:37 -0700

    add temp file creation function

--- src/Bio/Pipeline/Utils.hs
+++ src/Bio/Pipeline/Utils.hs
@@ -6,6 +6,8 @@ module Bio.Pipeline.Utils
     ( Directory
     , asDir
     , getPath
+    , withTempDir
+    , withTemp
     ) where
 
 import           Control.Monad.IO.Class
@@ -15,6 +17,9 @@ import           Data.String            (IsString (..))
 import qualified Data.Text              as T
 import           GHC.Generics           (Generic)
 import           Shelly                 (fromText, mkdir_p, shelly)
+import System.IO.Temp (withSystemTempDirectory, withTempDirectory, withSystemTempFile, withTempFile)
+import Control.Monad.Catch (MonadMask)
+import System.IO (hClose)
 
 newtype Directory = Directory FilePath deriving (Show, Read, Ord, Eq, Generic)
 
@@ -42,3 +47,21 @@ getPath :: MonadIO m => Directory -> m FilePath
 getPath (Directory dir) = liftIO $ shelly $
     mkdir_p (fromText $ T.pack dir) >> return dir
 {-# INLINE getPath #-}
+
+-- | Create a temporary directory in the given directory.
+withTempDir :: (MonadMask m, MonadIO m)
+            => Maybe FilePath   -- ^ Parent directory to create the directory in
+            -> (FilePath -> m a) -> m a
+withTempDir Nothing = withSystemTempDirectory "tmp_dir_"
+withTempDir (Just dir) = withTempDirectory dir "tmp_dir_"
+{-# INLINE withTempDir #-}
+
+-- | Create a temporary file in the given directory.
+withTemp :: (MonadMask m, MonadIO m)
+         => Maybe FilePath   -- ^ Parent directory to create the directory in
+         -> (FilePath -> m a) -> m a
+withTemp Nothing f = withSystemTempFile "tmp_file_" $ \fl h ->
+    liftIO (hClose h) >> f fl
+withTemp (Just dir) f = withTempFile dir "tmp_file_" $ \fl h ->
+    liftIO (hClose h) >> f fl
+{-# INLINE withTemp #-}
